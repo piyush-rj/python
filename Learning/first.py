a9e921389e6de54de992fcfc864a5b618a0abdf5
PiyushRaj2610 <piyushraj26102004@gmail.com>
2025-03-30 00:03:25 +0530

    packages

---
+++ Learning/first.py
@@ -0,0 +1,39 @@
+# datatypes -> bytes, int, float, string
+print("hello") 
+a = 20
+d = "piyush"
+print(d)
+
+e = None
+print(e)
+
+
+# typeCasting
+
+f = "5"
+print(f)
+
+print(int(f) + 1)
+
+g = 5
+print(str(g))
+
+# operators 
+
+# arithmetic operators
+# // -> int value
+# % -> remainder
+# / ->  value
+# (a += b) -> (a = a + b) 
+
+# comparison operators
+# >, <, !=, ==
+
+# logical operators
+# operator1 and operator2
+# operator1 or operator 2
+# not(false)
+
+print(not(False))
+
+# bitwise operators -> 
---
+++ Learning/packages/first.py
@@ -0,0 +1,6 @@
+import pandas as pd
+print(pd.__version__)
+
+
+df = pd.read_csv("/home/piyushr/ML/python/Learning/packages/Data.csv")
+print(df)
